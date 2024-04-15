63029217f2f288e54d547dc26f843a36ab8c3f09
JacobTheCreature <123994795+JacobTheCreature@users.noreply.github.com>
2024-04-14 20:46:22 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    qmlRegisterType<minesweepercell>("Minesweeper", 1, 0, "Minesweepercell"); // Connects the C++ class with QML
-    qmlRegisterType<difficultySelect>("difficultySelect", 1, 0, "DifficultySelect"); // Connects the C++ class with QML
+    qmlRegisterType<minesweepercell>("Minesweeper", 1, 0, "Minesweepercell"); // Connects the C++ class to QML
+    qmlRegisterType<difficultySelect>("difficultySelect", 1, 0, "DifficultySelect"); // Connects the C++ class to QML
 
     QQmlApplicationEngine engine;
     const QUrl url(u"qrc:/ihopethisworks/Main.qml"_qs);
